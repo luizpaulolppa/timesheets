6408d44724525b0db4cdcac14eac3409442d8566
Luiz Paulo Pilegi de Almeida <luizpilegilppa@gmail.com>
2020-07-19 20:14:44 -0300

    criada autenticação das rotas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,34 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
 import SignIn from '../pages/SignIn';
+import { isAuthenticated } from '../auth';
+
+const PrivateRouter = ({ component, ...rest }: any) => {
+  const Component = component;
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        isAuthenticated() ? (
+          <Component {...props} />
+        ) : (
+            <Redirect to={{ pathname: "/", state: { from: props.location } }} />
+          )
+      }
+    />
+  )
+}
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" component={() => (<SignIn action="signin" />)} exact />
-    <Route path="/dashboard" component={Dashboard} exact />
-    <Route path="/signin" component={() => (<SignIn action="signin" />)} exact />
-    <Route path="/new-account" component={() => (<SignIn action="new-account" />)} exact />
-    <Route path="/success-new-account" component={() => (<SignIn action="success-new-account" />)} exact />
-    <Route path="/forgot-password" component={() => (<SignIn action="forgot-password" />)} exact />
+    <Route exact path="/" component={() => (<SignIn action="signin" />)} />
+    <Route exact path="/signin" component={() => (<SignIn action="signin" />)} />
+    <Route exact path="/new-account" component={() => (<SignIn action="new-account" />)} />
+    <Route exact path="/success-new-account" component={() => (<SignIn action="success-new-account" />)} />
+    <Route exact path="/forgot-password" component={() => (<SignIn action="forgot-password" />)} />
+    <PrivateRouter exact path="/dashboard" component={Dashboard} />
   </Switch>
 );
 
